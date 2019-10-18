967eb01155f301517967832774df323162b2b425
hisashi.takagi <worldhistory14@gmail.com>
2019-10-18 12:33:37 +0000

    post

---
+++ resources/views/microposts/microposts.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    @if (Auth::check())
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
+<ul class="media-list">
+    @foreach ($microposts as $micropost)
+        <li class="media mb-3">
+            <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
+            <div class="media-body">
+                <div>
+                    {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                </div>
+                <div>
+                    <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                </div>
+                <div>
+                    @if (Auth::id() == $micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                        {!! Form::close() !!}
+                    @endif
+                </div>
+            </div>
+        </li>
+    @endforeach
+</ul>
+{{ $microposts->links('pagination::bootstrap-4') }}
